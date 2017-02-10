06b40c9cbe727ceceda211d87487a9279a084f7f
Omar Abdelrahman <o.3ammor@gmail.com>
2017-02-10 16:04:26 +0200

    creating the env class

---
+++ environment.py
@@ -0,0 +1,17 @@
+class Environment(object):
+	"""Wrapping the gym environment"""
+
+	def __init__(self, arg):
+		pass
+
+	def reset(self):
+		pass
+
+	def step(self, action):
+		pass
+
+	def submit(self):
+		pass
+
+	def state_processor(self, state):
+		pass
