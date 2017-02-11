07a7e6f8f63ffd923dffaee6ba075aa75198f77c
Omar Abdelrahman <o.3ammor@gmail.com>
2017-02-11 18:29:26 +0200

    format config

--- config.py
+++ config.py
@@ -1,28 +1,34 @@
 """This file is to put any configuration to our classes"""
 
+
 class EnvConfig():
-	env_name = 'Breakout-v0'
-	state_processor_params = { "resize_shape": (84, 84),
-						"crop_box": (34, 0, 160, 160),
-						"gray": True,
-						"frames_num": 4 }
-	monitor = True
-	record_video_every = 10
+    env_name = 'Breakout-v0'
+    state_processor_params = {"resize_shape": (84, 84),
+                              "crop_box": (34, 0, 160, 160),
+                              "gray": True,
+                              "frames_num": 4}
+    monitor = True
+    record_video_every = 10
+
 
 class AgentConfig():
-	bla = 'bla'
+    bla = 'bla'
+
 
 class ReplayMemoryConfig():
-	def __init__(self):
-		self.max_size=500000
+    def __init__(self):
+        self.max_size = 500000
+
 
 class EstimatorConfig():
-	def __init__(self):
-		self.name="blablabla"
-		self.learning_rate=.0002
+    def __init__(self):
+        self.name = "blablabla"
+        self.learning_rate = .0002
+
 
 class Expriment1(EnvConfig, AgentConfig, ReplayMemoryConfig, EstimatorConfig):
-	experiment_dir = "./expriment/"
+    experiment_dir = "./expriment/"
+
 
 def get_config():
-	return Expriment1
+    return Expriment1
