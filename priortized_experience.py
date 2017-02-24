8ff220b256e7fa3628e99ce0852ba7930e21ae79
Omar Abdelrahman <o.3ammor@gmail.com>
2017-02-24 17:38:37 +0200

    the PRM class

---
+++ priortized_experience.py
@@ -0,0 +1,23 @@
+class PrioritizedExperienceReplay:
+    "the PRM class"
+
+    def __init__(self):
+        pass
+
+    def set_bounries(self):
+        pass
+
+    def get_index(self):
+        pass
+
+    def push(self):
+        pass
+
+    def balance(self):
+        pass
+
+    def update_priority(self):
+        pass
+
+    def sample(self):
+        pass
