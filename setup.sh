d10b561eb503852afb58cf125ab1935d4f9dedfb
3ammor <o.3ammor@gmail.com>
2017-03-15 22:55:07 +0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock
+
 sudo apt-get install -y python-numpy python-dev cmake zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
+
+export LC_ALL=C
 
 git clone https://github.com/openai/gym.git
 cd gym
